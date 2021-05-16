aa9d395fb4f3d20c725d8ffcf43b8337bcac3704
qw753954 <68770475+qw753954@users.noreply.github.com>
2021-05-17 00:22:12 +0800

    first

---
+++ assets/js/all.js
@@ -0,0 +1,14 @@
+$(document).ready(() => {
+    $('.burgerBtn').click((e) => {
+        e.preventDefault();
+        $('.navbar').toggleClass('burgerBtn-active');
+    });
+
+    $('.favorite').each((i, item) => {
+        // console.log(item);
+        $(item).click(e => {
+            e.preventDefault();
+            $(item).toggleClass('active');
+        })
+    })
+});
